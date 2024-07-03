7c53814aad996821b7e4d140c028f4e98bcafb7c
Shashika Maduranga Rupasinghe <srupasinghe913@gmail.com>
2024-07-03 16:05:11 +0530

    first part

--- index.js
+++ index.js
@@ -2,7 +2,34 @@
 
 for(var i=0; i<document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-        var audio = new Audio("sounds/tom-1.mp3");
+        var drumletter = this.innerHTML;
+
+        switch(drumletter){
+            case "w":
+                var audio = new Audio("sounds/tom-1.mp3");
+                break;
+            case "a":
+                var audio = new Audio("sounds/tom-2.mp3");
+                break;
+            case "s":
+                var audio = new Audio("sounds/tom-3.mp3");
+                break;
+            case "d":
+                var audio = new Audio("sounds/tom-4.mp3");
+                break;
+            case "j":
+                var audio = new Audio("sounds/snare.mp3");
+                break;
+            case "k":
+                var audio = new Audio("sounds/crash.mp3");
+                break;
+            case "l":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                break;
+        }
         audio.play();
     });
 }
+
+/* var audio = new Audio("sounds/tom-1.mp3");
+        audio.play(); */
